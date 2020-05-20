9b2bbe6214e07acfde4172a1cd5ab1fa21acce1c
JaminB3n <64145160+JaminB3n@users.noreply.github.com>
2020-05-20 13:14:24 +0200

    tryouts

--- javascript/src/greetings/greetings.js
+++ javascript/src/greetings/greetings.js
@@ -8,8 +8,10 @@
 function greeting(parameterVariable) {
   // This line prints 'Hello, World!' to the console:
   console.log("Hello, World!");
+  
 
   // Write a line of code that prints parameterVariable to stdout using console.log:
+  console.log(parameterVariable);
 }
 
 export { greeting };
