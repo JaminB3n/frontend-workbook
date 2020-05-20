3b9d7c1f2218bce196b6cf23346f08bc208fe663
JaminB3n <64145160+JaminB3n@users.noreply.github.com>
2020-05-20 13:18:52 +0200

    factorial

--- javascript/src/factorial/factorial.js
+++ javascript/src/factorial/factorial.js
@@ -3,3 +3,20 @@
  *  example: 4!  = 4 * 3 * 2 * 1
  */
 
+function factorial(){
+    let res = 1;
+    let fac = 5;
+
+    for(i = 0; i < fac; i++){
+        res = res * (fac - i);
+        console.log(res);
+    }
+    
+}
+factorial();
+
+
+
+
+// Wanna try to get user input readline/prompt to calc factorial
+
